34c79ce85c397e55c83a0dfd07a3a656a4dbf309
Koson Trachu <koson.trachu@gmail.com>
2021-10-09 00:59:32 +0700

    Remove the now ignored directory

--- Debug/TouchGFX/generated/images/src/fadeyellowred_small.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/generated/images/src/fadeyellowred_small.o: \
- ../TouchGFX/generated/images/src/fadeyellowred_small.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
